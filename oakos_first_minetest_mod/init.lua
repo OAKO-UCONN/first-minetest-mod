edbf02abf84114f9dee2ef1b987c8f9dfcb26d52
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-11-24 22:06:42 -0500

    Crafting_Recipie_Setup

--- oakos_first_minetest_mod/init.lua
+++ oakos_first_minetest_mod/init.lua
@@ -11,3 +11,20 @@ minetest.register_node("oakos_first_mod:oakoblock", {
     groups = {oddly_breakable_by_hand = 2},
 })
 --]]
+
+minetest.register_craft({
+    output = "oakos_first_minetest_mod:oakoblock",
+    recipe = {{"default:dirt", "default:dirt"}}
+})
+
+--This code is from the Travelnet Elevators as an example of how crafting recipies work.
+--if( minetest.get_modpath("technic") ~= nil ) then
+--        minetest.register_craft({
+--                output = "travelnet:elevator",
+--		recipe = {
+--                        {"default:steel_ingot", "technic:motor", "default:steel_ingot", },
+--                	{"default:steel_ingot", "technic:control_logic_unit", "default:steel_ingot", },
+--                	{"default:steel_ingot", "moreores:copper_ingot", "default:steel_ingot", }
+--                }
+--        })
+--else
