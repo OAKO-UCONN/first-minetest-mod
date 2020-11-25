54d0f3bc677dfd80d2d84cf3f1d222b7281034ff
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-11-24 21:13:52 -0500

    Creating a new block item In Minetest.
    Created a Custom made texture as well.

---
+++ oakos_first_minetest_mod/init.lua
@@ -0,0 +1,5 @@
+minetest.register_node("oakos_first_mod:oakoblock", {
+    description = "OAKO's most expensive block in Minetest Universe!",
+    tiles = {"oakos_first_mod_oakoblock.png"},
+    groups = {oddly_breakable_by_hand = 2},
+})
