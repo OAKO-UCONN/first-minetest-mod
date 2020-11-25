5ed6d5d3aebcafdc49368989b71c67320de6c9f4
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-11-24 21:40:15 -0500

    Commenting Out code for Debug

--- oakos_first_minetest_mod/init.lua
+++ oakos_first_minetest_mod/init.lua
@@ -4,3 +4,10 @@ minetest.register_node("oakos_first_mod:oakoblock", {
     groups = {oddly_breakable_by_hand = 2},
 })
 
+--[[
+minetest.register_node("oakos_first_mod:oakoblock", {
+    description = "OAKO's most expensive block in Minetest Universe!",
+    tiles = {"oakos_first_mod_oakoblock.png"},
+    groups = {oddly_breakable_by_hand = 2},
+})
+--]]
